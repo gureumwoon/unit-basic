5e8a754019295165bf60a8076477ec50ec60a36d
gureumwoon <vfrvfr3221@gmail.com>
2024-06-14 22:55:21 +0900

    TDD 사용해서 test code 작성

---
+++ src/stack/test/stack.test.js
@@ -0,0 +1,51 @@
+const Stack = require('../stack.js');
+
+describe('Stack', () => {
+    let stack;
+
+    beforeEach(() => {
+        stack = new Stack();
+    });
+
+    it('is created empty', () => {
+        expect(stack.size()).toBe(0);
+    });
+
+    it('allows to push item', () => {
+        stack.push('🐼');
+
+        expect(stack.size()).toBe(1);
+    });
+
+    describe('pop', () => {
+        it('throws an error if stack is empty', () => {
+            expect(() => {
+                stack.pop();
+            }).toThrow('Stack is empty');
+        });
+
+        it('return the last pushed item and removed it from the stack', () => {
+            stack.push('🐼');
+            stack.push('🥕');
+
+            expect(stack.pop()).toBe('🥕');
+            expect(stack.size()).toBe(1);
+        })
+    });
+
+    describe('peek', () => {
+        it('throws an error if stack is empty', () => {
+            expect(() => {
+                stack.peek();
+            }).toThrow('Stack is empty');
+        });
+
+        it('returns the last pushed item but keeps it in the stack', () => {
+            stack.push('🐼');
+            stack.push('🥕');
+
+            expect(stack.peek()).toBe('🥕');
+            expect(stack.size()).toBe(2);
+        })
+    });
+});
